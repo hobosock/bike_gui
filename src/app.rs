c9b826eb94a34980aa47c67ee1ea66fcfba7596b
hobosock <76829496+hobosock@users.noreply.github.com>
2023-10-24 08:26:08 -0400

    having to rework the app for new library
    Needs to handle futures now, yay.

--- src/app.rs
+++ src/app.rs
@@ -5,7 +5,10 @@
 use crate::bluetooth::*;
 
 // external crates
-use btleplug::platform::{Adapter, Manager, Peripheral, PeripheralId};
+use btleplug::{
+    api::{Central, Peripheral as Peripheral_api},
+    platform::{Adapter, Manager, Peripheral, PeripheralId},
+};
 use eframe::egui;
 
 /*=======================================================================
@@ -26,22 +29,80 @@ pub struct BikeApp {
     // app state stuff
     active_tab: Tabs,
     // bluetooth stuff
-    bt_adapters: Vec<Adapter>,
-    peripheral_list: Vec<Peripheral>,
+    bt_adapters: Option<Vec<Adapter>>,
+    selected_adapter: Option<Adapter>,
+    selected_adapter_number: Option<usize>,
+    adapter_moved: bool,
+    adapter_text: String,
+    peripheral_list: Option<Vec<Peripheral>>,
+    selected_peripheral: Option<Peripheral>,
+    selected_peripheral_number: Option<usize>,
+    peripheral_moved: bool,
+    peripheral_text: String,
+    peripheral_connected: bool,
 }
 
 impl Default for BikeApp {
     fn default() -> Self {
         Self {
             active_tab: Tabs::Main,
-            bt_adapters: Vec::new(),
-            peripheral_list: Vec::new(),
+            bt_adapters: None,
+            selected_adapter: None,
+            selected_adapter_number: None,
+            adapter_moved: false,
+            adapter_text: "None selected".to_string(),
+            peripheral_list: None,
+            selected_peripheral: None,
+            selected_peripheral_number: None,
+            peripheral_moved: false,
+            peripheral_text: "None selected".to_string(),
+            peripheral_connected: false,
         }
     }
 }
 
 impl eframe::App for BikeApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // update state
+        if self.bt_adapters.is_some() && self.selected_adapter_number.is_some() {
+            if self.adapter_moved == false {
+                self.adapter_text = update_adapter_text(
+                    &self.bt_adapters.as_ref().unwrap()
+                        [self.selected_adapter_number.clone().unwrap()],
+                );
+                self.selected_adapter = Some(
+                    self.bt_adapters
+                        .as_mut()
+                        .unwrap()
+                        .remove(self.selected_adapter_number.clone().unwrap()),
+                );
+                self.adapter_moved = true;
+            }
+        }
+        if self.peripheral_list.is_some() && self.selected_peripheral_number.is_some() {
+            if self.peripheral_moved == false {
+                self.peripheral_text = update_peripheral_text(
+                    &self.peripheral_list.as_ref().unwrap()
+                        [self.selected_peripheral_number.clone().unwrap()],
+                );
+                self.selected_peripheral = Some(
+                    self.peripheral_list
+                        .as_mut()
+                        .unwrap()
+                        .remove(self.selected_peripheral_number.clone().unwrap()),
+                );
+                self.peripheral_moved = true;
+            }
+        }
+        if self.peripheral_moved && self.selected_peripheral.is_some() {
+            async {
+                match self.selected_peripheral.unwrap().is_connected().await {
+                    Ok(flag) => self.peripheral_connected = flag,
+                    Err(_) => {}
+                }
+            };
+        }
+
         // main window
         egui::TopBottomPanel::top("Tabs").show(ctx, |ui| {
             ui.horizontal(|ui| {
@@ -52,18 +113,150 @@ impl eframe::App for BikeApp {
             });
             ui.separator();
             match self.active_tab {
-                Tabs::Main => {}
+                Tabs::Main => {
+                    if self.peripheral_connected && self.selected_peripheral.is_some() {
+                        async {
+                            let peripheral = self.selected_peripheral.clone().unwrap();
+                            let discovered_services = peripheral.discover_services().await;
+                            let characteristics = peripheral.characteristics();
+                            for chars in characteristics.iter() {
+                                ui.add_sized(
+                                    ui.available_size(),
+                                    egui::TextEdit::singleline(&mut chars.uuid.to_string()),
+                                );
+                            }
+                        };
+                    }
+                }
                 Tabs::Workouts => {}
                 Tabs::Bluetooth => {
-                    if ui.button("Scan").clicked() {
-                        if self.bt_adapters.len() >= 1 {
-                            let adapter_options = bt_scan();
+                    ui.horizontal(|ui| {
+                        if ui.button("Adapter").clicked() {
+                            async {
+                                self.bt_adapters = bt_adapter_scan().await;
+                            };
                         }
-                    }
-                    egui::ComboBox::from_label("Choose a peripheral.")
+                        if self.adapter_moved {
+                            async {
+                                let adapter_info_str = String::new();
+                                match self.selected_adapter.clone().unwrap().adapter_info().await {
+                                    Ok(info) => adapter_info_str = info,
+                                    Err(e) => adapter_info_str = e.to_string(),
+                                }
+                                egui::ComboBox::from_label("Choose an adapter.")
+                                    .selected_text(&self.adapter_text)
+                                    .show_ui(ui, |ui| {
+                                        ui.selectable_value(
+                                            &mut self.selected_adapter_number,
+                                            Some(0 as usize),
+                                            adapter_info_str,
+                                        );
+                                    });
+                            };
+                        } else {
+                            async {
+                                egui::ComboBox::from_label("Choose an adapter.")
+                                    .selected_text(&self.adapter_text)
+                                    .show_ui(ui, |ui| match &self.bt_adapters {
+                                        Some(adapters) => {
+                                            for (i, adapter) in adapters.iter().enumerate() {
+                                                ui.selectable_value(
+                                                    &mut self.selected_adapter_number,
+                                                    Some(i),
+                                                    adapter.identifier().unwrap().to_string(),
+                                                );
+                                            }
+                                        }
+                                        None => {
+                                            ui.selectable_value(
+                                                &mut self.selected_adapter_number,
+                                                None,
+                                                "None",
+                                            );
+                                        }
+                                    });
+                            };
+                        }
+                    });
+                    ui.horizontal(|ui| {
+                        if ui.button("Scan").clicked() {
+                            if self.adapter_moved {
+                                println!("Scanning for devices...");
+                                self.peripheral_list =
+                                    bt_scan(&mut self.selected_adapter.as_mut().unwrap());
+                            }
+                        }
+                        if self.peripheral_moved {
+                            egui::ComboBox::from_label("Choose a device.")
+                                .selected_text(&self.peripheral_text)
+                                .show_ui(ui, |ui| {
+                                    ui.selectable_value(
+                                        &mut self.selected_peripheral_number,
+                                        Some(0 as usize),
+                                        self.selected_peripheral
+                                            .as_ref()
+                                            .unwrap()
+                                            .identifier()
+                                            .unwrap(),
+                                    );
+                                });
+                        } else {
+                            egui::ComboBox::from_label("Choose a device.")
+                                .selected_text(&self.peripheral_text)
+                                .show_ui(ui, |ui| match &self.peripheral_list {
+                                    Some(peripherals) => {
+                                        for (i, peripheral) in peripherals.iter().enumerate() {
+                                            let name_str: String;
+                                            if peripheral.identifier().unwrap().is_empty() {
+                                                name_str = "Unknown device".to_string();
+                                            } else {
+                                                name_str = peripheral.identifier().unwrap();
+                                                // could put UUID here or something?
+                                            }
+                                            ui.selectable_value(
+                                                &mut self.selected_peripheral_number,
+                                                Some(i),
+                                                name_str,
+                                            );
+                                        }
+                                    }
+                                    None => {
+                                        ui.selectable_value(
+                                            &mut self.selected_peripheral_number,
+                                            None,
+                                            "None",
+                                        );
+                                    }
+                                });
+                        }
+                        if ui.button("Connect").clicked() {
+                            if self.peripheral_moved {
+                                println!("Connecting to device...");
+                            }
+                        } else {
+                            println!("Please scan for devices and select one to connect");
+                        }
+                    });
                 }
                 Tabs::Help => {}
             }
         });
     }
+}
+
+/// update bluetooth adapter combobox text based on selection
+fn update_adapter_text(adapter: &Adapter) -> String {
+    let adapter_str = adapter.identifier().unwrap().to_string();
+    return adapter_str;
+}
+
+/// update bluetooth peripheral combobox text based on selection
+fn update_peripheral_text(peripheral: &Peripheral) -> String {
+    let peripheral_str: String;
+    if peripheral.identifier().unwrap().is_empty {
+        peripheral_str = "Unknown device".to_string();
+    } else {
+        peripheral_str = peripheral.identifier().unwrap().to_string();
+    }
+    return peripheral_str;
 }
